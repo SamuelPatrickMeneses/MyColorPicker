8776ecd8463c3632348e328820e6e0589d119066
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2021-07-23 15:40:30 -0300

    Parcial layout 1.0

---
+++ js/index.js
@@ -0,0 +1,12 @@
+(function () {
+    
+    window.addEventListener('load', function () {
+        /*$("#cabeçalho").load('header.html');
+        $("#rodapé").load('footer.html');
+        $("#main").load('home.html');*/
+        $(document).ready(function () {
+        $('select').formSelect();
+    });
+    })
+
+}());
